0e2f810f45a2ecfea11cafae9993cb86d3a38c68
annalindqvist <annalindqvist_@live.se>
2022-11-08 14:05:11 +0100

    mediaquerys styling and mediaquerys in javascript for menu

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const toggleClose = document.getElementById("toggle-close");
 const menuLinks = document.getElementById("menu-links")
 const allPages = document.getElementById("all-pages")
 let offset = 48;
+const windowSize = window.matchMedia("(max-width: 767px)")
 
 toggleMenu = () => {
     if (menuLinks.style.display === "flex"){
@@ -20,26 +21,34 @@ toggleMenu = () => {
     }
 }
 
-
 scrollToAboutMe = () => {
     const aboutMe = document.getElementById("about-me-page");
-    toggleMenu();
+
+    if (windowSize.matches) {
+        toggleMenu();
+    }
     window.scroll({ top: (aboutMe.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
 
 scrollToEducation = () => {
     const education = document.getElementById("education-page");
-    toggleMenu();
+    if (windowSize.matches) {
+        toggleMenu();
+    }
     window.scroll({ top: (education.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
 
 scrollToWorkExperience = () => {
     const workExperience = document.getElementById("work-experience-page");
-    toggleMenu();
+    if (windowSize.matches) {
+        toggleMenu();
+    }
     window.scroll({ top: (workExperience.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
 scrollToContact = () => {
     const contact = document.getElementById("contact-me-page");
-    toggleMenu();
+    if (windowSize.matches) {
+        toggleMenu();
+    }
     window.scroll({ top: (contact.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
