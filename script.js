efad9d6a24985efd0e9382322013082c5dc001fb
annalindqvist <annalindqvist_@live.se>
2022-11-08 12:23:04 +0100

    img on startpage and display flex fixes

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
 const toggle = document.getElementById("toggle");
 const toggleClose = document.getElementById("toggle-close");
 const menuLinks = document.getElementById("menu-links")
+const allPages = document.getElementById("all-pages")
+let offset = 48;
 
 toggleMenu = () => {
     if (menuLinks.style.display === "flex"){
         menuLinks.style.display = "none";
         toggle.style.display = "block";
         toggleClose.style.display = "none";
+        allPages.style.display ="block";
+        
     } else {
         menuLinks.style.display = "flex";
         toggleClose.style.display = "block";
         toggle.style.display = "none";
+        allPages.style.display = "none";
+     
     }
+}
+
+
+scrollToAboutMe = () => {
+    const aboutMe = document.getElementById("about-me-page");
+    toggleMenu();
+    window.scroll({ top: (aboutMe.offsetTop - offset), left: 0, behavior: 'smooth' });
+}
+
+scrollToEducation = () => {
+    const education = document.getElementById("education-page");
+    toggleMenu();
+    window.scroll({ top: (education.offsetTop - offset), left: 0, behavior: 'smooth' });
+}
+
+scrollToWorkExperience = () => {
+    const workExperience = document.getElementById("work-experience-page");
+    toggleMenu();
+    window.scroll({ top: (workExperience.offsetTop - offset), left: 0, behavior: 'smooth' });
+}
+scrollToContact = () => {
+    const contact = document.getElementById("contact-me-page");
+    toggleMenu();
+    window.scroll({ top: (contact.offsetTop - offset), left: 0, behavior: 'smooth' });
 }
